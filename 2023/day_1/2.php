8114df47c5a09bc084b49717d59770598f993bd4
Elliot Derhay <elliot@elliotderhay.com>
2023-12-02 18:09:01 -0500

    Day 2 part 2 complete

--- 2023/day_1/2.php
+++ 2023/day_1/2.php
@@ -5,13 +5,22 @@ $input = fopen(__DIR__ . '/input', 'r');
 $nums = [];
 
 while ($line = fgets($input)) {
-    // echo print_r($line, true);
+	preg_match_all('/(?=([1-9]|one|two|three|four|five|six|seven|eight|nine))/m', $line, $matches);
 
-    $digits = str_split(preg_replace('/([^0-9]|one|two|three|four|five|six|seven|eight|nine)/m', '', $line));
+	$digits = array_map(static fn ($item) => match($item) {
+			'one' => '1',
+			'two' => '2',
+			'three' => '3',
+			'four' => '4',
+			'five' => '5',
+			'six' => '6',
+			'seven' => '7',
+			'eight' => '8',
+			'nine' => '9',
+			default => $item,
+		}, $matches[1]);
 
-    // echo print_r("{$digits}\n", true);
-
-    $nums[] = (int) ($digits[0] . $digits[count($digits) - 1]);
+	$nums[] = (int) ($digits[0] . $digits[count($digits) - 1]);
 }
 
-print_r(array_sum($nums) . "\n");
+echo array_sum($nums) . "\n";
